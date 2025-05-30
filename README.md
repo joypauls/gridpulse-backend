b66b5233bcdd5d3c070c6c85276abfe4282e6cc7
joypauls <joypaulsen3@gmail.com>
2025-05-29 20:07:47 -0500

    add github action

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # dailygrid-backend
+
+## Development
+
+### Local
+
+Requires .env file or environment variable set
--- dailygrid_backend/config.py
+++ dailygrid_backend/config.py
@@ -5,7 +5,8 @@ load_dotenv()
 
 EIA_API_KEY = os.getenv("EIA_API_KEY")
 EIA_BASE_URL = "https://api.eia.gov/v2/electricity/rto/daily-fuel-type-data/data/"
-OUTPUT_FILE = "data/daily_energy_mix_latest.json"
+# OUTPUT_FILE = "data/daily_energy_mix_latest.json"
+OUTPUT_FILE = "public/data/daily_energy_mix_latest.json"
 
 if not EIA_API_KEY:
     raise RuntimeError("EIA_API_KEY not found in environment")
