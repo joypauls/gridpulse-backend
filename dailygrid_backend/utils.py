d467d4c578fe0968a569adb1a21fa852f84ee099
joypauls <joypaulsen3@gmail.com>
2025-05-30 19:46:35 -0500

    use central time for everything for now

--- dailygrid_backend/utils.py
+++ dailygrid_backend/utils.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone, timedelta
+from datetime import datetime, timedelta
 import pytz
 
 
@@ -8,12 +8,14 @@ def get_now_central_string() -> str:
 
 def get_today_string() -> str:
     """Returns today's date in YYYY-MM-DD format."""
-    return datetime.now(timezone.utc).strftime("%Y-%m-%d")
+    return datetime.now(pytz.timezone("US/Central")).strftime("%Y-%m-%d")
 
 
 def get_minus_n_days_string(n: int) -> str:
     """Returns the date n days ago in YYYY-MM-DD format."""
-    return (datetime.now(timezone.utc) - timedelta(days=n)).strftime("%Y-%m-%d")
+    return (datetime.now(pytz.timezone("US/Central")) - timedelta(days=n)).strftime(
+        "%Y-%m-%d"
+    )
 
 
 def get_yesterday_string() -> str:
